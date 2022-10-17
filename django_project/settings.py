076076f5190cab80861e9dbfd6e54763276a84f0
Jonathan <114219464+Auca-XT@users.noreply.github.com>
2022-10-17 01:41:13 +0000

    random tag, random joke add home

--- django_project/settings.py
+++ django_project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'common.apps.CommonConfig',
     'jokes.apps.JokesConfig',
     'pages.apps.PagesConfig',
 ]
